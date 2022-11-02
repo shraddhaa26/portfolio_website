fd38899d2c5ff56185aa84960de4b6d3dd9e89bc
shraddhaa26 <110374043+shraddhaa26@users.noreply.github.com>
2022-11-02 21:07:39 +0530

    Update script.js

--- script.js
+++ script.js
@@ -35,14 +35,14 @@ $(document).ready(function(){
 
     // typing text animation script
     var typed = new Typed(".typing", {
-        strings: ["Developer", "Dancer", "Writer", "Singer", "Traveller"],
+        strings: ["Developer", "Writer", "Singer", "Traveller"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
     });
 
     var typed = new Typed(".typing-2", {
-        strings: ["Developer", "Dancer", "Writer", "Singer", "Traveller"],
+        strings: ["Developer", "Writer", "Singer", "Traveller"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
